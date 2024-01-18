d642d5dea29e6a57ea7e1b5e601c311eb8dc5da7
Darsh <Darshjain2310@gmail.com>
2024-01-18 19:36:27 +0530

    18 jan update

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 document.getElementsByClassName("random")[0].onclick=function(){
     i=Math.random()
-    document.getElementsByClassName("start")[0].textContent=parseInt(i*6+1)
+    x = parseInt(i*6+1)
+    document.getElementsByClassName("start")[0].textContent= x
+    document.getElementsByClassName("random")[0].disabled=true;
     
-};
+count=0
 document.getElementsByClassName("random1")[0].onclick=function(){
+    count++
+    document.getElementsByClassName("click")[0].textContent=count
     i=Math.random()
-    document.getElementsByClassName("second")[0].textContent=parseInt(i*6+1)
+    y = parseInt(i*6+1)
+    document.getElementsByClassName("second")[0].textContent=y
+
+    if(x==y){
+        document.getElementsByClassName("random1")[0].disabled=true;
+    }
+}
 }
