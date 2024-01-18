da35104690169478e878f12c9243a468d812792a
Darsh <Darshjain2310@gmail.com>
2024-01-18 20:12:52 +0530

    18 jan last update

---
+++ player2.js
@@ -0,0 +1,19 @@
+document.getElementsByClassName("random2")[0].onclick=function(){
+    i=Math.random()
+    x = parseInt(i*6+1)
+    document.getElementsByClassName("start1")[0].textContent= x
+    document.getElementsByClassName("random2")[0].disabled=true;
+    
+count=0
+document.getElementsByClassName("random3")[0].onclick=function(){
+    count++
+    document.getElementsByClassName("click1")[0].textContent=count
+    i=Math.random()
+    y = parseInt(i*6+1)
+    document.getElementsByClassName("second1")[0].textContent=y
+
+    if(x==y){
+        document.getElementsByClassName("random3")[0].disabled=true;
+    }
+}
+}
