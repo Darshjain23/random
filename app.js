a4e22148e778f855f7c1f22fc37c4eb2b871b674
Darsh <Darshjain2310@gmail.com>
2024-01-25 19:00:39 +0530

    25 jan update2

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+document.getElementsByClassName("random")[0].disabled=false;
+document.getElementsByClassName("random1")[0].disabled=true;
 document.getElementsByClassName("random")[0].onclick=function(){
     i=Math.random()
     x = parseInt(i*6+1)
     document.getElementsByClassName("start")[0].textContent= x
     document.getElementsByClassName("random")[0].disabled=true;
-    
-
-count=0
+    document.getElementsByClassName("random1")[0].disabled=false;
+}
+sum=0
 document.getElementsByClassName("random1")[0].onclick=function(){
-    count++
-    document.getElementsByClassName("click")[0].textContent=count
+    sum++
+    document.getElementsByClassName("click")[0].textContent=sum
     i=Math.random()
     y = parseInt(i*6+1)
     document.getElementsByClassName("second")[0].textContent=y
@@ -17,5 +19,4 @@ document.getElementsByClassName("random1")[0].onclick=function(){
         document.getElementsByClassName("random1")[0].disabled=true;
         document.getElementsByClassName("random2")[0].disabled=false;
     }
-}
 }
--- player2.js
+++ player2.js
@@ -1,10 +1,12 @@
 document.getElementsByClassName("random2")[0].disabled=true;
+document.getElementsByClassName("random3")[0].disabled=true;
 document.getElementsByClassName("random2")[0].onclick=function(){
     i=Math.random()
     x = parseInt(i*6+1)
     document.getElementsByClassName("start1")[0].textContent= x
     document.getElementsByClassName("random2")[0].disabled=true;
-    
+    document.getElementsByClassName("random3")[0].disabled=false;
+} 
 count=0
 document.getElementsByClassName("random3")[0].onclick=function(){
     count++
@@ -15,6 +17,21 @@ document.getElementsByClassName("random3")[0].onclick=function(){
 
     if(x==y){
         document.getElementsByClassName("random3")[0].disabled=true;
+
+        a=parseInt(document.getElementsByClassName("click")[0].textContent)
+        b=parseInt(document.getElementsByClassName("click1")[0].textContent)
+    
+        if(a>b){
+            document.getElementsByClassName("ply1")[0].textContent="You are looser"
+            document.getElementsByClassName("ply2")[0].textContent="You are winner"
+        }
+    
+        else{
+            document.getElementsByClassName("ply1")[0].textContent="You are looser"
+            document.getElementsByClassName("ply2")[0].textContent="You are winner"
+        }
+
     }
 }
-}
+
+
