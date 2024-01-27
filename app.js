c7f27ddcec65cb8c8111a4a34aa7cf610b9cd6a5
Darsh <Darshjain2310@gmail.com>
2024-01-27 16:53:40 +0530

    player 2 update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.getElementsByClassName("random")[0].disabled=false;
 document.getElementsByClassName("random1")[0].disabled=true;
 document.getElementsByClassName("random")[0].onclick=function(){
     i=Math.random()
-    x = parseInt(i*6+1)
+    x = parseInt(i*3+1)
     document.getElementsByClassName("start")[0].textContent= x
     document.getElementsByClassName("random")[0].disabled=true;
     document.getElementsByClassName("random1")[0].disabled=false;
@@ -12,7 +12,7 @@ document.getElementsByClassName("random1")[0].onclick=function(){
     sum++
     document.getElementsByClassName("click")[0].textContent=sum
     i=Math.random()
-    y = parseInt(i*6+1)
+    y = parseInt(i*3+1)
     document.getElementsByClassName("second")[0].textContent=y
 
     if(x==y){
--- player2.js
+++ player2.js
@@ -2,7 +2,7 @@ document.getElementsByClassName("random2")[0].disabled=true;
 document.getElementsByClassName("random3")[0].disabled=true;
 document.getElementsByClassName("random2")[0].onclick=function(){
     i=Math.random()
-    x = parseInt(i*6+1)
+    x = parseInt(i*3+1)
     document.getElementsByClassName("start1")[0].textContent= x
     document.getElementsByClassName("random2")[0].disabled=true;
     document.getElementsByClassName("random3")[0].disabled=false;
@@ -12,7 +12,7 @@ document.getElementsByClassName("random3")[0].onclick=function(){
     count++
     document.getElementsByClassName("click1")[0].textContent=count
     i=Math.random()
-    y = parseInt(i*6+1)
+    y = parseInt(i*3+1)
     document.getElementsByClassName("second1")[0].textContent=y
 
     if(x==y){
@@ -20,15 +20,22 @@ document.getElementsByClassName("random3")[0].onclick=function(){
 
         a=parseInt(document.getElementsByClassName("click")[0].textContent)
         b=parseInt(document.getElementsByClassName("click1")[0].textContent)
+        console.log(a)
+        console.log(b)
     
         if(a>b){
             document.getElementsByClassName("ply1")[0].textContent="You are looser"
             document.getElementsByClassName("ply2")[0].textContent="You are winner"
         }
+
+        else if(a==b){
+            document.getElementsByClassName("ply1")[0].textContent="Tie"
+            document.getElementsByClassName("ply2")[0].textContent="Tie"
+        }
     
         else{
-            document.getElementsByClassName("ply1")[0].textContent="You are looser"
-            document.getElementsByClassName("ply2")[0].textContent="You are winner"
+            document.getElementsByClassName("ply1")[0].textContent="You are winner"
+            document.getElementsByClassName("ply2")[0].textContent="You are looser"
         }
 
     }
